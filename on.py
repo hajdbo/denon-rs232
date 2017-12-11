9f7fbc4c7649a0748170bf5ba41f06229b8b6dcb
Boris Hajduk <boris.hajduk@gmail.com>
2017-12-11 22:48:26 +0800

    better timeouts

--- on.py
+++ on.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python
 
 import os
-import time
 import sys
 import denon
-import cgi
-import re
 
 def main():
 
   denonctl = denon.Denon('/dev/ttyS0',1)
   denonctl.what_si()
   denonctl.power_status()
-  if (denonctl.power == "OFF"):
+  if (denonctl.power == u"OFF"):
     denonctl.power_on()
     denonctl.mv(45)
-  if (denonctl.si != "DBS"):
+  if (denonctl.si != u"DBS"):
     denonctl.si_dbs()
 
 if __name__ == '__main__':
